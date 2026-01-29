e4ec8a4c3cac403b5bf6177bc41c6f1102d80b9b
SpringSecurity <geodb@naver.com>
2026-01-29 21:52:03 +0900

    'update'

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://192.168.0.2.nip.io:8080";
+//export const API_BASE_URL = "http://192.168.0.2.nip.io:8080";
+export const API_BASE_URL = "https://192.168.0.2.nip.io:8443";
