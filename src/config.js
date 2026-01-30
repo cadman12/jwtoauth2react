481f2a2e259af84ebc5c07b479cca941ca976230
Lee, Sanghyun <65234370+cadman12@users.noreply.github.com>
2026-01-30 12:33:48 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = "http://192.168.0.2.nip.io:8080";
-export const API_BASE_URL = "https://192.168.0.2.nip.io:8443";
+export const API_BASE_URL = "https://192.168.0.7.nip.io:8443";
